cec3012bb1d2f1a4e99bb3738e8b3840ac40cb1a
Dennelle <dennelle.walton@gmail.com>
2023-11-09 06:34:31 -0500

    react-app-tutorial

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+
+//PascalCasing - the convention expected to follow.
+function Message() {
+//JSX: Javascript XML
+    const name = 'Dennelle';
+    if (name)
+    return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+export default Message;
